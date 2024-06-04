9f0e71a3109e692d80c1e9624840ac4d9153e2e2
Mwantok Kwoopnaan <kwoopnaan.mwantok@binghamuni.edu.ng>
2024-06-04 14:02:39 +0100

    Algorithms

---
+++ Swap.java
@@ -0,0 +1,18 @@
+public class Swap {
+
+
+    public static void main(String[] args) {
+        int number = 78;
+        int anotherNumber = 36;
+        int temp = 0;
+
+        temp = number;
+        number = anotherNumber;
+        anotherNumber = temp;
+
+
+        System.out.println(number);
+        System.out.println(anotherNumber);
+
+    }
+}
